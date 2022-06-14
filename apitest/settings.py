8bc7afe7d56ab8155a4fb9fba27f75523996a136
speed1test <edwin12sv@gmail.com>
2022-06-13 19:46:07 -0600

    actualizando el host

--- apitest/settings.py
+++ apitest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zou2*5ai8y8jpljod-4j*=4vembq4q*cujq=)6-c%6c@+an)jr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
